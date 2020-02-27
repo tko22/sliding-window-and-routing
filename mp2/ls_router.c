f081353ec5ad59e698c4c157d9f00d853c412aaa
Timothy Ko <timothy.l.ko@gmail.com>
2020-02-27 13:10:54 -0600

    add initial Makefile

---
+++ mp2/ls_router.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
