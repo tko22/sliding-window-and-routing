43684868229598e5bc6bc892b4d65d965b6e3ca6
Timothy Ko <tk2@illionois.edu>
2020-04-14 16:06:55 -0700

    better paramters for ls

--- mp2/ls_router.cpp
+++ mp2/ls_router.cpp
@@ -80,7 +80,7 @@ void lslistenForNeighbors()
             for (int x = 0; x < 256; x++)
             {
                 // last time you heard from node x (which you already had a connection with) is more than 1 seconds
-                if ((now.tv_sec - globalLastHeartbeat[x].tv_sec) >= 1 && connections[x] == true)
+                if ((now.tv_sec - globalLastHeartbeat[x].tv_sec) >= 1.8 && connections[x] == true)
                 {
                     std::cout << "\n"
                               << "DOWN NODE ~~~~~~ link with " << x << " is down..." << endl;
@@ -91,7 +91,7 @@ void lslistenForNeighbors()
                 }
             }
 
-            if (now.tv_sec - floodInterval.tv_sec > 5)
+            if (now.tv_sec - floodInterval.tv_sec > 6)
             {
                 // flood periodically
                 std::cout << "flooding periodically" << endl;
